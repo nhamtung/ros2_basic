ab990850fd82a59f4de831795a84b2fbf9f95b4d
nhamtung <nhamtung125@gmail.com>
2021-06-27 10:13:07 +0000

    create topic_py_pkg

--- topic_pkg/src/talker.cpp
+++ topic_pkg/src/talker.cpp
@@ -1,8 +1,9 @@
+// https://docs.ros.org/en/foxy/Tutorials/Writing-A-Simple-Cpp-Publisher-And-Subscriber.html
+
 #include <chrono>
 #include <functional>
 #include <memory>
 #include <string>
-
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
@@ -15,19 +16,16 @@ class MinimalPublisher : public rclcpp::Node
 {
   public:
     MinimalPublisher()
-    : Node("minimal_publisher"), count_(0)
-    {
+    : Node("minimal_publisher"), count_(0){
       publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-      timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
+      timer_ = this->create_wall_timer(500ms, std::bind(&MinimalPublisher::timer_callback, this));
     }
 
   private:
-    void timer_callback()
-    {
+    void timer_callback(){
       auto message = std_msgs::msg::String();
       message.data = "Hello, world! " + std::to_string(count_++);
-      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+      RCLCPP_INFO(this->get_logger(), "talker.cpp - Publishing: '%s'", message.data.c_str());
       publisher_->publish(message);
     }
     rclcpp::TimerBase::SharedPtr timer_;
