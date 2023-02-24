bae64e2e10d514c1120e5ebeaa63c818c7e5043d
nhamtung <nhamtung125@gmail.com>
2023-02-24 14:32:56 +0700

    topic_cpp

--- topic_cpp_pkg/src/listener.cpp
+++ topic_cpp_pkg/src/listener.cpp
@@ -9,13 +9,13 @@ using std::placeholders::_1;
 class MinimalSubscriber : public rclcpp::Node
 {
   public:
-    MinimalSubscriber() : Node("minimal_subscriber"){
+    MinimalSubscriber() : Node("listener"){
       subscription_ = this->create_subscription<std_msgs::msg::String>("topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
     }
 
   private:
     void topic_callback(const std_msgs::msg::String::SharedPtr msg) const{
-      RCLCPP_INFO(this->get_logger(), "listener.cpp - I heard: '%s'", msg->data.c_str());
+      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
     }
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
--- topic_cpp_pkg/src/talker.cpp
+++ topic_cpp_pkg/src/talker.cpp
@@ -15,7 +15,7 @@ using namespace std::chrono_literals;
 class MinimalPublisher : public rclcpp::Node
 {
   public:
-    MinimalPublisher() : Node("minimal_publisher"), count_(0){
+    MinimalPublisher() : Node("talker"), count_(0){
       publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
       timer_ = this->create_wall_timer(500ms, std::bind(&MinimalPublisher::timer_callback, this));
     }
@@ -24,7 +24,7 @@ class MinimalPublisher : public rclcpp::Node
     void timer_callback(){
       auto message = std_msgs::msg::String();
       message.data = "Hello, world! " + std::to_string(count_++);
-      RCLCPP_INFO(this->get_logger(), "talker.cpp - Publishing: '%s'", message.data.c_str());
+      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
       publisher_->publish(message);
     }
     rclcpp::TimerBase::SharedPtr timer_;
