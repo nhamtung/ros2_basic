c5811f6273a7c20382ce0ff1e107ed301e574c20
nhamtung <nhamtung125@gmail.com>
2023-03-20 17:48:41 +0700

    sing stamped datatypes with tf2_ros::MessageFilter

--- ros2_tf2/learning_tf2_py/setup.py
+++ ros2_tf2/learning_tf2_py/setup.py
@@ -28,6 +28,7 @@ setup(
             'turtle_tf2_listener = learning_tf2_py.turtle_tf2_listener:main',
             'fixed_frame_tf2_broadcaster = learning_tf2_py.fixed_frame_tf2_broadcaster:main',
             'dynamic_frame_tf2_broadcaster = learning_tf2_py.dynamic_frame_tf2_broadcaster:main',
+            'turtle_tf2_message_broadcaster = learning_tf2_py.turtle_tf2_message_broadcaster:main',
         ],
     },
 )
